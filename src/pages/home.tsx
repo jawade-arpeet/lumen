b2200d61cf825e3277d67340aa5a4251ef9282c6
jawade.arpeet <jawade.arpeet@gmail.com>
2025-01-17 14:39:01 +0530

    refactor: add logo to header

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,20 +1,14 @@
 import { Link } from "react-router-dom";
-import { SiLoom } from "react-icons/si";
 
 import Button from "../components/button";
+import Logo from "../components/logo";
 
 const Home = () => {
   return (
     <main>
       <header className="px-4 py-4 sm:px-6 sm:py-6 max-w-screen-xl mx-auto">
         <nav className="flex items-center justify-between">
-          <Link
-            to={"/"}
-            className="inline-flex items-center justify-center gap-x-2"
-          >
-            <SiLoom className="text-lime-400" size={24}/>
-            <span className="text-xl font-semibold">Lumen</span>
-          </Link>
+          <Logo />
           <div className="flex items-center gap-x-2 sm:gap-x-4">
             <Link to={"/sign-in"}>
               <Button variant={"ghost"}>Login</Button>
