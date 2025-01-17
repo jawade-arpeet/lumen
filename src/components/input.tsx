c018477074cefe5447bda37b4d75c4934f771b82
jawade.arpeet <jawade.arpeet@gmail.com>
2025-01-17 14:12:35 +0530

    feat: add reusable input component

---
+++ src/components/input.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import cn from "../utils/cn";
+
+const Input: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = ({
+  className,
+  ...props
+}) => {
+  return (
+    <input
+      className={cn(
+        "w-full px-3 py-2 h-9 font-medium text-sm border border-stone-400 placeholder:font-normal bg-transparent rounded-md focus:outline-none focus:ring-2 focus:ring-stone-400 focus:border-none placeholder:text-stone-400",
+        className,
+      )}
+      {...props}
+    />
+  );
+};
+
+export default Input;
