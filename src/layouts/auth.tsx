789a6784e73b9ee7899827b8064412fee2d86847
jawade.arpeet <jawade.arpeet@gmail.com>
2025-01-18 17:38:57 +0530

    feat: add hero section with CTA

--- src/layouts/auth.tsx
+++ src/layouts/auth.tsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
 
+import Logo from "../components/logo";
+
 const AuthLayout = () => {
   return (
     <main className="h-screen w-screen flex flex-col items-center justify-center">
       <section className={"w-4/5 max-w-sm"}>
+        <Logo/>
         <Outlet />
       </section>
     </main>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
+import { ArrowUpToLine } from "lucide-react";
 
 import Button from "../components/button";
 import Logo from "../components/logo";
 
 const Home = () => {
   return (
-    <main>
+    <main className="h-screen w-screen">
       <header className="px-4 py-4 sm:px-6 sm:py-6 max-w-screen-xl mx-auto">
         <nav className="flex items-center justify-between">
           <Logo />
@@ -19,6 +20,21 @@ const Home = () => {
           </div>
         </nav>
       </header>
+      <section className="mt-20 md:mt-24 px-4 md:px-10">
+        <h1 className="text-7xl md:text-8xl font-bold tracking-tight max-w-screen-md">
+          Share Your Stories Through Video
+        </h1>
+        <p className="text-sm text-stone-600 mt-1 max-w-lg sm:text-lg sm:leading-[1.4rem]">
+          Upload, stream, and share your videos with our growing community.
+          High-quality streaming, no compromises.
+        </p>
+        <Link to={"/sign-up"}>
+          <Button className="mt-4">
+            <ArrowUpToLine size={16} />
+            <span>Start Uploading</span>
+          </Button>
+        </Link>
+      </section>
     </main>
   );
 };
