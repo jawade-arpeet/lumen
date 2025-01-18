ce553039f6568acb7471925141405c90ac6ba973
jawade.arpeet <jawade.arpeet@gmail.com>
2025-01-18 16:02:08 +0530

    feat: add tanstack query setup and provider

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,26 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import AuthLayout from "./layouts/auth.tsx";
 import Home from "./pages/home.tsx";
 import SignIn from "./pages/sign-in.tsx";
-import AuthLayout from "./layouts/auth.tsx";
 import SignUp from "./pages/sign-up.tsx";
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route element={<AuthLayout />}>
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route element={<AuthLayout />}>
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 };
 
